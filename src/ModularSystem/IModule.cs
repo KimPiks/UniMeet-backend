79bfea87e19b36ec0f9c6d68a9454395c0753b8f
Kamil Prorok <kamilxprorok@gmail.com>
2025-10-30 16:41:11 +0100

    feat: modular system

---
+++ src/ModularSystem/IModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ModularSystem;
+
+public interface IModule
+{
+    string Name { get; init; }
+    bool Enabled { get; set; }
+
+    void Start(IConfiguration configuration);
+    void RegisterServices(IServiceCollection services);
+}
