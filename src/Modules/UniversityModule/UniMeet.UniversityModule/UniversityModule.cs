3364847c637dc4dc92f7df439c2e7e60165e706a
Kamil Prorok <kamilxprorok@gmail.com>
2025-11-02 13:46:36 +0100

    chore: build module structure

---
+++ src/Modules/UniversityModule/UniMeet.UniversityModule/UniversityModule.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ModularSystem;
+
+namespace UniMeet.UniversityModule;
+
+public class UniversityModule : IModule
+{
+    public string Name { get; init; } = "UniversityModule";
+    public bool Enabled { get; set; }
+    
+    public void Start(IConfiguration configuration)
+    {
+        this.Enabled = true;
+    }
+
+    public void RegisterServices(IServiceCollection services)
+    {
+        
+    }
+}
