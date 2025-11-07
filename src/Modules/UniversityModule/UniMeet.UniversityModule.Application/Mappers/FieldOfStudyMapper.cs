60f340e4961d2859cccc020e7aab7cf6888fce44
Kamil Prorok <kamilxprorok@gmail.com>
2025-11-07 20:57:46 +0100

    refactor: mappers for dto

---
+++ src/Modules/UniversityModule/UniMeet.UniversityModule.Application/Mappers/FieldOfStudyMapper.cs
@@ -0,0 +1,16 @@
+﻿using UniMeet.UniversityModule.Application.DTOs;
+using UniMeet.UniversityModule.Domain.Aggregates.UniversityAggregate;
+
+namespace UniMeet.UniversityModule.Application.Mappers;
+
+public static class FieldOfStudyMapper
+{
+    public static FieldOfStudyDto ToDto(this FieldOfStudy fieldOfStudy)
+    {
+        return new FieldOfStudyDto
+        {
+            Id = fieldOfStudy.Id,
+            Name = fieldOfStudy.Name
+        };
+    }
+}
