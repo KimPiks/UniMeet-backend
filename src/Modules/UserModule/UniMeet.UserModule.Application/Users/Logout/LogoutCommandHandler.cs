ddc6f24950ec83b0d90697b39be8f33b9d37e8fb
Kamil Prorok <kamilxprorok@gmail.com>
2025-12-26 20:46:58 +0100

    feat: logging out

---
+++ src/Modules/UserModule/UniMeet.UserModule.Application/Users/Logout/LogoutCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using UniMeet.Shared.Abstractions;
+using UniMeet.UserModule.Domain.RefreshTokens;
+using UniMeet.UserModule.Domain.RefreshTokens.Exceptions;
+
+namespace UniMeet.UserModule.Application.Users.Logout;
+
+public class LogoutCommandHandler(IRefreshTokenRepository refreshTokenRepository) : ICommandHandler<LogoutCommand>
+{
+    public async Task HandleAsync(LogoutCommand request, CancellationToken cancellationToken = default)
+    {
+        var token = await refreshTokenRepository.GetByTokenAsync(request.RefreshToken, cancellationToken);
+        if (token == null)
+            throw new TokenNotFoundException(request.RefreshToken);
+        
+        refreshTokenRepository.Delete(token);
+        await refreshTokenRepository.SaveChangesAsync(cancellationToken);
+    }
+}
